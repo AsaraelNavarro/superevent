3d12fa36d551a16dbca7cd847517f7074374ff44
Asarael Navarro <asarael.navarro@gmail.com>
2022-04-15 16:34:54 -0500

    add:index.html,header y footer

---
+++ js/jet-menu-integration-themes-generatepress-assets-js-script.js
@@ -0,0 +1,6 @@
+( function( $ ) {
+	jQuery( '.jet-menu' ).on( 'jetMenuCreated', function() {
+		$( this ).closest( '.main-navigation' ).removeClass( 'main-navigation' );
+	} );
+}( jQuery ) );
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */

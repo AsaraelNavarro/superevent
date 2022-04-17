821b6cc600cb85cc3e51ebfd66710872eb5d7f99
Asarael Navarro <asarael.navarro@gmail.com>
2022-04-16 23:19:12 -0500

    delete: Archivos innecesarios

--- js/jet-menu-integration-themes-generatepress-assets-js-script.js
+++
@@ -1,6 +0,0 @@
-( function( $ ) {
-	jQuery( '.jet-menu' ).on( 'jetMenuCreated', function() {
-		$( this ).closest( '.main-navigation' ).removeClass( 'main-navigation' );
-	} );
-}( jQuery ) );
-/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
